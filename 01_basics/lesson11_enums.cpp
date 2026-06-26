31e40ec5a7e4b0d55e6dae9dbbcfb395eabd551e
zaidBouallala <zaidbouallala7@gmail.com>
2026-06-26 22:04:21 +0100

    Add lesson 11 demonstrating enumerations in C++

---
+++ 01_basics/lesson11_enums.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+enum Color {Red, Green, Yellow, Blue};
+enum Direction {North, South, East, west};
+enum Week {Sat, Sun, Mon, Tue, Wed, Thu, Fri};
+enum Gendor {Male, Female};
+enum Status {Single, Married};
+
+
+int main()
+{
+    Color myColor;
+    Direction myDirection;
+    Week Tody;
+    Status myStatus;
+
+
+    myColor = Color::Yellow;
+
+    myDirection = Direction::South;
+
+    Tody = Week::Thu;
+
+    myStatus = Status::Single;
+
+
+}
+

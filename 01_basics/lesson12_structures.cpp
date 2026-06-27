ad85d69782c15864183098bd55661f99be38c523
zaidBouallala <zaidbouallala7@gmail.com>
2026-06-27 14:08:37 +0100

    Refactor Car structure to include stAddress and improve address handling

--- 01_basics/lesson12_structures.cpp
+++ 01_basics/lesson12_structures.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 
+struct stAddress
+{
+    string Street;
+    string POBOX;
+    string City;
+    string Country;
+};
+
 struct stOwner
 {
     string FullName;
     string Phone;
-};   
+    stAddress Address;
+}; 
+
 
 
 struct Car{
@@ -27,7 +37,10 @@ int main()
     MyCar1.Year = 2000;
     MyCar1.Owner.FullName = "zaid Bouallala";
     MyCar1.Owner.Phone = "+2126028192555";
-
+    MyCar1.Owner.Address.Street = "123 Main Street";
+    MyCar1.Owner.Address.POBOX = "456";
+    MyCar1.Owner.Address.City = "Casablanca";
+    MyCar1.Owner.Address.Country = "Morocco";
 
 
     MyCar2.Brand = "Ford";
@@ -35,13 +48,22 @@ int main()
     MyCar2.Year = 2022;
     MyCar2.Owner.FullName = "zaid Bouallala";
     MyCar2.Owner.Phone = "+2126028192555";
+    MyCar2.Owner.Address.Street = "789 Oak Avenue";
+    MyCar2.Owner.Address.POBOX = "101";
+    MyCar2.Owner.Address.City = "Rabat";
+    MyCar2.Owner.Address.Country = "Morocco";
 
     //cin>>MyCar1.Year;
 
     cout <<  MyCar1.Brand  <<endl<< MyCar1.Model  <<endl<<  MyCar1.Year <<endl ;
+    cout << MyCar1.Owner.Address.Street << " " << MyCar1.Owner.Address.POBOX << " " << MyCar1.Owner.Address.City << " " << MyCar1.Owner.Address.Country << endl;
     cout << MyCar1.Owner.FullName << " "  << MyCar1.Owner.Phone <<endl;
 
     cout <<  MyCar2.Brand  <<endl<< MyCar2.Model  <<endl<<  MyCar2.Year <<endl;
+    cout << MyCar2.Owner.Address.Street << " " << MyCar2.Owner.Address.POBOX << " " << MyCar2.Owner.Address.City << " " << MyCar2.Owner.Address.Country << endl;
     cout << MyCar2.Owner.FullName << " "  << MyCar2.Owner.Phone <<endl;
 
+
+
+    return 0;
 }

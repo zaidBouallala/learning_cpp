dbb0f300173ab1d52f33fd5e8de3d418b2e715c9
zaidBouallala <zaidbouallala7@gmail.com>
2026-06-27 14:08:29 +0100

    Fix enum definitions and variable names for clarity and consistency

--- 01_basics/lesson11_enums.cpp
+++ 01_basics/lesson11_enums.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 using namespace std;
 
-enum Color {Red, Green, Yellow, Blue};
-enum Direction {North, South, East, west};
-enum Week {Sat, Sun, Mon, Tue, Wed, Thu, Fri};
-enum Gendor {Male, Female};
-enum Status {Single, Married};
+enum Color {Red=100, Green=200, Yellow=300, Blue=400};
+enum Direction {North=1, South=2, East=3, West=4};
+enum Week {Sat=1, Sun=2, Mon=3, Tue=4, Wed=5, Thu=6, Fri=7};
+enum Gender {Male=1, Female=2};
+enum Status {Single=1, Married=2};
 
 
 int main()
 {
     Color myColor;
     Direction myDirection;
-    Week Tody;
+    Week today;
     Status myStatus;
 
 
     myColor = Color::Yellow;
 
-    myDirection = Direction::South;
+    myDirection = Direction::North; 
 
-    Tody = Week::Thu;
+    today = Week::Thu;
+
 
     myStatus = Status::Single;
+    
+
+
+    return 0;
 
 
 }

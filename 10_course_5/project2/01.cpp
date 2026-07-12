649de3013721d604fd871567be7abdd9cf5b4e1e
zaidBouallala <zaidbouallala7@gmail.com>
2026-07-12 22:58:08 +0100

    Add initial implementation of enums and includes in project2 C++ file

---
+++ 10_course_5/project2/01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+using namespace std;
+
+enum enDifLevel{Easy = 1 , Med = 2 , Hard = 3 , Mix = 4};
+enum enOpType{ add = 1, Sub = 2, Mult = 3, Div = 4, mix = 5 };
+enum enAnswerTrueOrFalse {True= 1 , False = 2};
+enum enWinner {Pass = 1 , Fail = 2};

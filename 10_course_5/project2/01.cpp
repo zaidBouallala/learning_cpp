22dd73c17bf79ebf8300e2f249e14acd0612ba2e
zaidBouallala <zaidbouallala7@gmail.com>
2026-07-12 22:58:55 +0100

    Add structures for round information and final game results

--- 10_course_5/project2/01.cpp
+++ 10_course_5/project2/01.cpp
@@ -7,3 +7,24 @@ enum enDifLevel{Easy = 1 , Med = 2 , Hard = 3 , Mix = 4};
 enum enOpType{ add = 1, Sub = 2, Mult = 3, Div = 4, mix = 5 };
 enum enAnswerTrueOrFalse {True= 1 , False = 2};
 enum enWinner {Pass = 1 , Fail = 2};
+
+struct stRoundInfo
+{
+    enDifLevel PlayerChoiceDifLvl;
+    enOpType PlayerChoiceOpType;
+    short Number1 = 0;
+    short Number2 = 0;
+    short Answer = 0;
+    enAnswerTrueOrFalse Ans_TrueOrFalse;
+};
+
+struct stResultFinalGame
+{
+    short RoundGame = 0;
+    string QuetionsLevel = "";
+    string OpertionType = "";
+    short NumberOfRightAnswer = 0;
+    short NumberOfWrongAnswer = 0;
+    enWinner Winner;
+    string TextPassOrLose = "";
+};

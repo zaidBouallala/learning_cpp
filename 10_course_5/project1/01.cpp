68a17ad71d9666c6d91c0f0322c6f4dc53b9ee98
zaidBouallala <zaidbouallala7@gmail.com>
2026-07-13 23:35:48 +0100

    Add RandomNumber and Rounds functions to handle game logic and user input

--- 10_course_5/project1/01.cpp
+++ 10_course_5/project1/01.cpp
@@ -7,3 +7,22 @@ enum enRPS {Stone=1 , Paper=2 , Scissors=3};
  int countMyWin=0;
  int countComputerWin=0;
  int countDraw=0;
+
+ int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+int Rounds()
+{   
+    int round;
+    cout<<"How many rounds 1 to 10 ?"<<endl;
+    do 
+    {
+    cin >> round;
+    } while(round < 1 || round > 10);
+  
+    return round;
+}
+

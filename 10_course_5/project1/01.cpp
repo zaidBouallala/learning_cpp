8bd7779eaa84a65a81a9c56bc6ef96d1f25b1d6a
zaidBouallala <zaidbouallala7@gmail.com>
2026-07-13 23:34:19 +0100

    Add initial setup for Rock-Paper-Scissors game with enums and win counters

---
+++ 10_course_5/project1/01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+enum enRPS {Stone=1 , Paper=2 , Scissors=3};
+ int countMyWin=0;
+ int countComputerWin=0;
+ int countDraw=0;

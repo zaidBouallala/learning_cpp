83dbf61dc2a159c7336b7ed0a3fe8c27a81352f8
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-07-21 19:22:47 +0100

    Refactor main function to include MYLib Test and input reading for dynamic summation

--- 11_course_6/create-Library/usingMyFirstLb.cpp
+++ 11_course_6/create-Library/usingMyFirstLb.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "myFirestLb.h"
+#include "myInputLib.h"
+
+using namespace std;
 
 int main()
 {
-    MYLib::Print();
+    MYLib::Test();
+
+    cout << "Sum of 10 + 20 = " << MYLib::sum(10,20) << endl;
+
+    int num1 = myInputLib::readNumber();
+    int num2 = myInputLib::readNumber();
+
+    cout << "Sum of " << num1 << " + " << num2 << " = " << MYLib::sum(num1,num2) << endl;
 }

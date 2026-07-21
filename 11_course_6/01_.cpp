f391d8ed0c781574af760c74788942bbeb3dd30c
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-07-21 19:22:40 +0100

    Add main function implementation with MYLib and math operations

--- 11_course_6/01_.cpp
+++ 11_course_6/01_.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include "mylab.h"
+#include "my-libs/myReadLib.h"
+#include "my-libs/myMathLib.h"
+
 using namespace std;
 int main()
 {
  MYLib::Print();
+
+    int number = myReadLib::ReadPositiveNumber("Please enter a positive number: ");
+    cout << "You entered: " << number << endl;
+
+    float num = 5.7;
+    cout << "Fractional part of " << num << " is: " << MyMathLib::getFractionalPart(num) << endl;
+    cout << "Absolute value of -10 is: " << MyMathLib::myAbs(-10) << endl;
+    cout << "Rounded value of 5.7 is: " << MyMathLib::myRound(5.7) << endl;
+    cout << "Floor value of 5.7 is: " << MyMathLib::myFloor(5.7) << endl;
+    cout << "Ceil value of 5.7 is: " << MyMathLib::myCeil(5.7) << endl;
+    cout << "Square root of 25 is: " << MyMathLib::mySqrt(25) << endl;
+
+    return 0;
+
+
 }

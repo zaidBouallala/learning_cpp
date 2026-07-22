9f1f4dbace0a29eee0e2ec8133102a2b4bac2727
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-07-22 18:23:58 +0100

    Add initial implementation of ranged for loop to display array elements

---
+++ 11_course_6/03_rangedLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int arr[] = { 10, 20, 30, 40, 50 };
+
+    for (int element : arr)
+    {
+        cout << element << endl;
+    }
+
+
+    return 0;
+}

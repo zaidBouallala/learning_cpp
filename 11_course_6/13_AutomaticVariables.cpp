184c5bed717c612b6dca4b75203ed5207b110a9b
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-07-23 18:00:21 +0100

    Add implementation of automatic variables demonstration

---
+++ 11_course_6/13_AutomaticVariables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+auto x = 10; //Type Integer
+auto y = 12.5; //Type Double
+auto z = "Mohammed Abu-Hadhoud"; //Type String
+cout << x << endl;
+cout << y << endl;
+cout << z << endl;
+return 0;
+}

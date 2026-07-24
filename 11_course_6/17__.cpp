973fe2ff08bd77e85fa9339196ed8f90085fa3ea
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-07-24 17:19:31 +0100

    Refactor variable name from 'c' to 'ch' for clarity in width setting demonstration

--- 11_course_6/17__.cpp
+++ 11_course_6/17__.cpp
@@ -7,12 +7,12 @@ char SchoolName[] = "Programming Advices";
 // print string and String
 printf("Dear %s, How are you?\n\n", Name);
 printf("Welcome to %s School!\n\n", SchoolName);
-char c = 'S';
-printf("Setting the width of c :%*c \n", 1, c);
-printf("Setting the width of c :%*c \n", 2, c);
-printf("Setting the width of c :%*c \n", 3, c);
-printf("Setting the width of c :%*c \n", 4, c);
-printf("Setting the width of c :%*c \n", 5, c);
+char ch = 'S';
+printf("Setting the width of c :%*c \n", 1, ch);
+printf("Setting the width of c :%*c \n", 2, ch);
+printf("Setting the width of c :%*c \n", 3, ch);
+printf("Setting the width of c :%*c \n", 4, ch );
+printf("Setting the width of c :%*c \n", 5, ch );
 return 0;
 }
 

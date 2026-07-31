fd63ed913f7353184f75809c5e3a47803c905730
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-07-31 23:26:10 +0100

    Add implementation of matrix operations including matrix printing and scalar matrix check in 01_Problem14.cpp

---
+++ 12_course_7/01_Problem14.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+for (short i = 0; i < Rows; i++)
+{
+for (short j = 0; j < Cols; j++)
+{
+//printf(" %0*d ", 2, arr[i][j]);
+cout << setw(3) << arr[i][j] << " ";
+}
+cout << "\n";
+}
+}
+bool IsScalarMarix(int Matrix1[3][3], short Rows, short Cols)
+{
+int FirstDiagElemement = Matrix1[0][0];
+for (short i = 0; i < Rows; i++)
+{
+for (short j = 0; j < Cols; j++)
+{
+//check for diagonals element
+if (i == j && Matrix1[i][j] != FirstDiagElemement)
+{
+return false;
+}
+//check for rest elements
+else if (i != j && Matrix1[i][j] != 0)
+{
+return false;
+}
+}
+}
+return true;
+}
+
+
+int main()
+{
+int Matrix1[3][3] = { {9,0,0},{0,9,0},{0,0,9} };
+cout << "\nMatrix1:\n";
+PrintMatrix(Matrix1, 3, 3);
+if (IsScalarMarix(Matrix1, 3, 3))
+cout << "\nYES: Matrix is scalar.";
+else
+cout << "\nNo: Matrix is NOT scalar.";
+system("pause>0");
+}

0bfdeff28474e976db80eb201f6285f50eaf1a77
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-08-24 12:13:38 +0100

    Add 01_Problem38.cpp for string trimming functionality

---
+++ 12_course_7/01_Problem38.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <iostream>
+using namespace std;
+string TrimLeft(string S1)
+{
+for (short i = 0; i < S1.length(); i++)
+{
+if (S1[i] != ' ')
+{
+return S1.substr(i, S1.length() - i);
+}
+}
+return "";
+}
+string TrimRight(string S1)
+{
+for (short i = S1.length() - 1; i >= 0; i--)
+{
+if (S1[i] != ' ')
+{
+return S1.substr(0, i + 1);
+}
+}
+return "";
+}
+string Trim(string S1)
+{
+return (TrimLeft(TrimRight(S1)));
+}
+ProgrammingAdvices.com
+© Copyright 2022
+Problem # 38/3 Solution Using C++
+int main()
+{
+string S1 = " Mohammed Abu-Hahdoud ";
+cout << "\nString = " << S1;
+cout << "\n\nTrim Left = " << TrimLeft(S1);
+cout << "\nTrim Right = " << TrimRight(S1);
+cout << "\nTrim = " << Trim(S1);
+system("pause>0");
+}

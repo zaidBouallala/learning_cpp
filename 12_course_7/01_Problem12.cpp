ac2ed0a24db82d8fa149e5316c0b145e41ce0d25
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-02 22:20:17 +0100

    Refactor matrix functions for improved readability and consistency

--- 12_course_7/01_Problem12.cpp
+++ 12_course_7/01_Problem12.cpp
@@ -1,67 +1,81 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+
 using namespace std;
+
 int RandomNumber(int From, int To)
 {
-//Function to generate a random number
-int randNum = rand() % (To - From + 1) + From;
-return randNum;
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
 }
-void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short
-Cols)
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
 {
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-arr[i][j] = RandomNumber(1, 10);
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            arr[i][j] = RandomNumber(1, 10);
+        }
+    }
 }
-}
-}
+
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-printf(" %0*d ", 2, arr[i][j]);
-//cout << setw(3) << arr[i][j] << " ";
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            printf(" %0*d ", 2, arr[i][j]);
+            //cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
-cout << "\n";
-}
+
+bool AreTepicalEqualMatrices(int Matrix1[3][3], int Matrix2[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (Matrix1[i][j] != Matrix2[i][j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 
-bool AreTypicalMatrices(int Matrix1[3][3], int Matrix2[3][3],
-short Rows, short Cols)
-{
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-if (Matrix1[i][j] != Matrix2[i][j])
-{
-return false;
-}
-}
-}
-return true;
-}
+
+
+
+
 int main()
 {
-//Seeds the random number generator in C++, called only once
-srand((unsigned)time(NULL));
-int Matrix1[3][3], Matrix2[3][3];
-FillMatrixWithRandomNumbers(Matrix1, 3, 3);
-cout << "\nMatrix1:\n";
-PrintMatrix(Matrix1, 3, 3);
-FillMatrixWithRandomNumbers(Matrix2, 3, 3);
-cout << "\nMatrix2:\n";
-PrintMatrix(Matrix2, 3, 3);
-if (AreTypicalMatrices(Matrix1, Matrix2, 3, 3))
-cout << "\nYES: both martices are typical.";
-else
-cout << "\nNo: martices are NOT typical.";
-system("pause>0");
+    srand((unsigned)time(NULL));
+
+    int arr1[3][3] , arr2[3][3];
+    FillMatrixWithRandomNumbers(arr1, 3, 3);
+    cout << "\nMatrix1:\n";
+    PrintMatrix(arr1, 3, 3);
+
+    FillMatrixWithRandomNumbers(arr2, 3, 3);
+    cout << "\nMatrix2:\n";
+    PrintMatrix(arr2, 3, 3);
+
+    if (AreTepicalEqualMatrices(arr1, arr1, 3, 3))
+    {
+        cout << "\nThe two matrices are equal.\n";
+    }
+    else
+    {
+        cout << "\nThe two matrices are not equal.\n";
+    }
+
+    system("pause 0");
+    return 0;
 }

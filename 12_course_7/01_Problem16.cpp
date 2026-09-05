fdfef4b8d6bb299ea0c57d304a3b5bb6aa77c8b4
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-05 20:24:36 +0100

    Refactor matrix functions: consolidate zero counting and sparse matrix checks

--- 12_course_7/01_Problem16.cpp
+++ 12_course_7/01_Problem16.cpp
@@ -16,37 +16,38 @@ cout << setw(3) << arr[i][j] << " ";
 cout << "\n";
 }
 }
-short CountNumberInMatrix(int Matrix1[3][3], int Number, short
-Rows, short Cols)
+
+int countZeroInMatrix(int arr[3][3], short Rows, short Cols)
 {
-short NumberCount = 0;
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-if (Matrix1[i][j] == Number)
-{
-NumberCount++;
-};
+    int count = 0;
+
+    for (short i = 0 ;i < Rows ; i++){
+        for(short j =0 ; j< Cols ; j++)
+        {
+            if(arr[i][j] == 0) count++;
+        }
+    }
+    return count;
 }
-}
-return NumberCount;
-}
-bool IsSparseMatrix(int Matrix1[3][3], short Rows, short Cols)
+
+void isSparseMatrix(int arr[3][3], short Rows, short Cols)
 {
-short MatrixSize = Rows * Cols;
-return (CountNumberInMatrix(Matrix1, 0, 3, 3) >= ceil((float) MatrixSize / 2));
+    int count = countZeroInMatrix(arr, Rows, Cols);
+    if(count > (Rows * Cols) / 2)
+    {
+        cout << "\nYES: Matrix is sparse.";
+    }else{
+        cout << "\nNO: Matrix is NOT sparse.";
+    }
 }
 
 int main()
 {
-int Matrix1[3][3] = { {0,0,12},{9,9,1},{0,0,9} };
+int Matrix1[3][3] = { {0,0,12},{9,0,1},{0,0,9} };
 cout << "\nMatrix1:\n";
 PrintMatrix(Matrix1, 3, 3);
-if (IsSparseMatrix(Matrix1, 3, 3))
-cout << "\nYes: It is Sparse\n";
-else
-cout << "\nNo: It's NOT Sparse\n";
+isSparseMatrix(Matrix1, 3, 3);
+
 system("pause>0");
 }
 

012a86fe79789cc992e9d252e30e110b814bbbd1
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-10 18:46:52 +0100

    Fix indentation for improved readability in ReadString and PrintEachWordInString functions

--- 12_course_7/01_Problem40.cpp
+++ 12_course_7/01_Problem40.cpp
@@ -4,29 +4,29 @@
 using namespace std;
 string JoinString(vector<string> vString, string Delim)
 {
-string S1="";
-for (string& s : vString)
-{
-S1 = S1 + s + Delim;
+    string S1 = "";
+    for (string &s : vString)
+    {
+        S1 = S1 + s + Delim;
+    }
+    return S1.substr(0, S1.length() - Delim.length());
 }
-return S1.substr(0,S1.length()-Delim.length());
-}
-string JoinString(string arrString[],short Length, string Delim)
+string JoinString(string arrString[], short Length, string Delim)
 {
-string S1 = "";
-for (short i=0;i< Length;i++)
-{
-S1 = S1 + arrString[i] + Delim;
-}
-return S1.substr(0, S1.length() - Delim.length());
+    string S1 = "";
+    for (short i = 0; i < Length; i++)
+    {
+        S1 = S1 + arrString[i] + Delim;
+    }
+    return S1.substr(0, S1.length() - Delim.length());
 }
 int main()
 {
-vector<string> vString = { "Mohammed","Faid","Ali","Maher" };
-string arrString[]= { "Mohammed","Faid","Ali","Maher" };
-cout << "\nVector after join: \n";
-cout << JoinString(vString, " ");
-cout << "\n\nArray after join: \n";
-cout << JoinString(arrString,4, " ");
-system("pause>0");
+    vector<string> vString = {"Mohammed", "Faid", "Ali", "Maher"};
+    string arrString[] = {"Mohammed", "Faid", "Ali", "Maher"};
+    cout << "\nVector after join: \n";
+    cout << JoinString(vString, " ");
+    cout << "\n\nArray after join: \n";
+    cout << JoinString(arrString, 4, " ");
+    system("pause>0");
 }

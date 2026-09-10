c53e3c77b54af7bf846027e6a5503d1c053421fe
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-10 18:46:47 +0100

    Fix indentation for improved readability in ReadString and PrintEachWordInString functions

--- 12_course_7/01_Problem39.cpp
+++ 12_course_7/01_Problem39.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 string JoinString(vector<string> vString, string Delim)
 {
-string S1="";
-for (string& s : vString)
-{
-S1 = S1 + s + Delim;
-}
-return S1.substr(0,S1.length()-Delim.length());
+    string S1 = "";
+    for (string &s : vString)
+    {
+        S1 = S1 + s + Delim;
+    }
+    return S1.substr(0, S1.length() - Delim.length());
 }
 int main()
 {
-vector<string> vString = { "Mohammed","Faid","Ali","Maher" };
-cout << "\nVector after join: \n";
-cout << JoinString(vString, " ");
-system("pause>0");
+    vector<string> vString = {"Mohammed", "Faid", "Ali", "Maher"};
+    cout << "\nVector after join: \n";
+    cout << JoinString(vString, " ");
+    system("pause>0");
 }

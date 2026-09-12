3964196163246b7be9f3b9fdfd6b754a3e332584
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-12 14:11:20 +0100

    Fix indentation for improved readability in PrintMatrix and IsScalarMarix functions

--- 12_course_7/01_Problem14.cpp
+++ 12_course_7/01_Problem14.cpp
@@ -4,49 +4,51 @@
 using namespace std;
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-//printf(" %0*d ", 2, arr[i][j]);
-cout << setw(3) << arr[i][j] << " ";
-}
-cout << "\n";
-}
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            // printf(" %0*d ", 2, arr[i][j]);
+            cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
 bool IsScalarMarix(int Matrix[3][3], short Rows, short Cols)
 {
     int firstElement = Matrix[0][0];
-    for(short i = 0;i < Rows; i++){
-        for(short j = 0;j < Cols ; j++){
-            if(i == j){
-                if(Matrix[i][j] != firstElement){
-                    return false;
-                }
-            }else{
-                if(Matrix[i][j] != 0){
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (i == j)
+            {
+                if (Matrix[i][j] != firstElement)
+                {
                     return false;
                 }
             }
-
-
-
+            else
+            {
+                if (Matrix[i][j] != 0)
+                {
+                    return false;
+                }
+            }
         }
     }
     return true;
 }
 
-
 int main()
 {
-int Matrix1[3][3] = { {9,0,0},{0,9,0},{0,0,9} };
-cout << "\nMatrix1:\n";
-PrintMatrix(Matrix1, 3, 3);
-if (IsScalarMarix(Matrix1, 3, 3))
-cout << "\nYES: Matrix is scalar.";
-else
-cout << "\nNo: Matrix is NOT scalar.";
+    int Matrix1[3][3] = {{9, 0, 0}, {0, 9, 0}, {0, 0, 9}};
+    cout << "\nMatrix1:\n";
+    PrintMatrix(Matrix1, 3, 3);
+    if (IsScalarMarix(Matrix1, 3, 3))
+        cout << "\nYES: Matrix is scalar.";
+    else
+        cout << "\nNo: Matrix is NOT scalar.";
 
-
-system("pause>0");
+    system("pause>0");
 }

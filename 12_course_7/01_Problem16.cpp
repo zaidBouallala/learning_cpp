f42810bf14355e11b6d103f5224392abc401c701
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-12 14:11:55 +0100

    Fix indentation for improved readability in PrintMatrix, countZeroInMatrix, and isSparseMatrix functions

--- 12_course_7/01_Problem16.cpp
+++ 12_course_7/01_Problem16.cpp
@@ -6,24 +6,26 @@
 using namespace std;
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-//printf(" %0*d ", 2, arr[i][j]);
-cout << setw(3) << arr[i][j] << " ";
-}
-cout << "\n";
-}
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            // printf(" %0*d ", 2, arr[i][j]);
+            cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
 int countZeroInMatrix(int arr[3][3], short Rows, short Cols)
 {
     int count = 0;
 
-    for (short i = 0 ;i < Rows ; i++){
-        for(short j =0 ; j< Cols ; j++)
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
         {
-            if(arr[i][j] == 0) count++;
+            if (arr[i][j] == 0)
+                count++;
         }
     }
     return count;
@@ -31,20 +33,20 @@ int countZeroInMatrix(int arr[3][3], short Rows, short Cols)
 void isSparseMatrix(int arr[3][3], short Rows, short Cols)
 {
     int count = countZeroInMatrix(arr, Rows, Cols);
-    if(count > (Rows * Cols) / 2)
+    if (count > (Rows * Cols) / 2)
     {
         cout << "\nYES: Matrix is sparse.";
-    }else{
+    }
+    else
+    {
         cout << "\nNO: Matrix is NOT sparse.";
     }
 }
 int main()
 {
-int Matrix1[3][3] = { {0,0,12},{9,0,1},{0,0,9} };
-cout << "\nMatrix1:\n";
-PrintMatrix(Matrix1, 3, 3);
-isSparseMatrix(Matrix1, 3, 3);
-system("pause>0");
+    int Matrix1[3][3] = {{0, 0, 12}, {9, 0, 1}, {0, 0, 9}};
+    cout << "\nMatrix1:\n";
+    PrintMatrix(Matrix1, 3, 3);
+    isSparseMatrix(Matrix1, 3, 3);
+    system("pause>0");
 }
-
-

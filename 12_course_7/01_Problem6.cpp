deba52db0a65b1f5d7bd0911e7a3314f6a46cf6d
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-12 10:37:59 +0100

    Fix indentation for improved readability in FillMatrixWithOrderedNumbers and PrintMatrix functions

--- 12_course_7/01_Problem6.cpp
+++ 12_course_7/01_Problem6.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short
-Cols)
+void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short Cols)
 {
-short Counter = 0;
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-Counter++;
-arr[i][j] = Counter;
-}
-}
+    short Counter = 0;
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            Counter++;
+            arr[i][j] = Counter;
+        }
+    }
 }
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-cout << arr[i][j] << " ";
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
-cout << "\n";
-}
-}
-
 
 int main()
 {
-int arr[3][3];
-FillMatrixWithOrderedNumbers(arr, 3, 3);
-cout << "\nThe following is a 3x3 ordered matrix:\n";
-PrintMatrix(arr, 3, 3);
-system("pause>0");
+    int arr[3][3];
+    FillMatrixWithOrderedNumbers(arr, 3, 3);
+    cout << "\nThe following is a 3x3 ordered matrix:\n";
+    PrintMatrix(arr, 3, 3);
+    system("pause>0");
 }

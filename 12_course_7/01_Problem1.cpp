79e996e194a6de12f8db15d4e088c60882089e20
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-12 10:37:16 +0100

    Fix indentation for improved readability in RandomNumber, FillMatrixWithRandomNumbers, PrintMatrix, and main functions

--- 12_course_7/01_Problem1.cpp
+++ 12_course_7/01_Problem1.cpp
@@ -3,7 +3,6 @@
 // #include <iomanip>
 // #include <cstdlib>
 
-
 // using namespace std;
 
 // int RandomNumber(int from, int to)
@@ -20,9 +19,7 @@
 //         }
 //     }
 
-
 // }
-
 
 // void printTheMatrix(int arr[3][3]){
 //     cout << "The following is a 3x3 random matrix : " << endl;
@@ -34,8 +31,6 @@
 //     }
 
 // }
-
-
 
 // int main()
 // {
@@ -54,42 +49,40 @@
 using namespace std;
 int RandomNumber(int From, int To)
 {
-//Function to generate a random number
-int randNum = rand() % (To - From + 1) + From;
-return randNum;
+    // Function to generate a random number
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
 }
-void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short
-Cols)
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
 {
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-arr[i][j] = RandomNumber(1, 100);
-}
-}
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
 }
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
-for (short i = 0; i < Rows; i++)
-{
-for (short j = 0; j < Cols; j++)
-{
-cout <<setw(3) << arr[i][j] << " ";
-}
-cout << "\n";
-}
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
 
 int main()
 {
-//Seeds the random number generator in C++, called only once
-srand((unsigned)time(NULL));
+    // Seeds the random number generator in C++, called only once
+    srand((unsigned)time(NULL));
 
-
-int arr[3][3];
-FillMatrixWithRandomNumbers(arr, 3, 3);
-cout << "\n The following is a 3x3 random matrix:\n";
-PrintMatrix(arr, 3, 3);
-system("pause>0");
+    int arr[3][3];
+    FillMatrixWithRandomNumbers(arr, 3, 3);
+    cout << "\n The following is a 3x3 random matrix:\n";
+    PrintMatrix(arr, 3, 3);
+    system("pause>0");
 }

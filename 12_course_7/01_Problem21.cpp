ef8ec92c2e4692767a0ed4bfcdbb978ec6ede800
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-13 14:33:54 +0100

    Fix indentation for improved readability in PrintFibonacciUsingLoop function

--- 12_course_7/01_Problem21.cpp
+++ 12_course_7/01_Problem21.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 void PrintFibonacciUsingLoop(short Number)
 {
-int FebNumber = 0;
-int Prev2 = 0, Prev1 = 1;
-cout << "1 ";
-for (short i = 2; i <= Number; ++i)
-{
-FebNumber = Prev1 + Prev2;
-cout << FebNumber << " ";
-Prev2 = Prev1;
-Prev1 = FebNumber;
-}
+    int FebNumber = 0;
+    int Prev2 = 0, Prev1 = 1;
+    cout << "1 ";
+    for (short i = 2; i <= Number; ++i)
+    {
+        FebNumber = Prev1 + Prev2;
+        cout << FebNumber << " ";
+        Prev2 = Prev1;
+        Prev1 = FebNumber;
+    }
 }
 int main()
 {
-PrintFibonacciUsingLoop(10);
-system("pause>0");
+    PrintFibonacciUsingLoop(10);
+    system("pause>0");
 }

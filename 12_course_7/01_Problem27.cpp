7c24ece3a58bf4d1eac3e1daaf02a1a15cb03012
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-16 22:52:37 +0100

    Fix indentation for improved readability in ReadChar, InvertLetterCase, and main functions

--- 12_course_7/01_Problem27.cpp
+++ 12_course_7/01_Problem27.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 char ReadChar()
 {
-char Ch1;
-cout << "Please Enter a Character?\n";
-cin >> Ch1;
-return Ch1;
+    char Ch1;
+    cout << "Please Enter a Character?\n";
+    cin >> Ch1;
+    return Ch1;
 }
 char InvertLetterCase(char char1)
 {
-return isupper(char1) ? tolower(char1) : toupper(char1);
+    return isupper(char1) ? tolower(char1) : toupper(char1);
 }
 int main()
 {
-char Ch1 = ReadChar();
-cout << "\nChar after inverting case:\n";
-Ch1 = InvertLetterCase(Ch1);
-cout << Ch1 << endl;
-system("pause>0");
+    char Ch1 = ReadChar();
+    cout << "\nChar after inverting case:\n";
+    Ch1 = InvertLetterCase(Ch1);
+    cout << Ch1 << endl;
+    system("pause>0");
 }

a92c049aa88f7b3dfabc4e01ca95c7cee75f682c
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-16 22:52:29 +0100

    Fix indentation for improved readability in ReadString, UpperAllString, and LowerAllString functions

--- 12_course_7/01_Problem26.cpp
+++ 12_course_7/01_Problem26.cpp
@@ -3,36 +3,36 @@
 using namespace std;
 string ReadString()
 {
-string S1;
-cout << "Please Enter Your String?\n";
-getline(cin, S1);
-return S1;
+    string S1;
+    cout << "Please Enter Your String?\n";
+    getline(cin, S1);
+    return S1;
 }
 string UpperAllString(string S1)
 {
-    short poss =S1.length(); 
-for (short i = 0; i < poss; i++)
-{
-S1[i] = toupper(S1[i]);
-}
-return S1;
+    short poss = S1.length();
+    for (short i = 0; i < poss; i++)
+    {
+        S1[i] = toupper(S1[i]);
+    }
+    return S1;
 }
 string LowerAllString(string S1)
 {
-for (short i = 0; i < S1.length(); i++)
-{
-S1[i] = tolower(S1[i]);
-}
-return S1;
+    for (short i = 0; i < S1.length(); i++)
+    {
+        S1[i] = tolower(S1[i]);
+    }
+    return S1;
 }
 int main()
 {
-string S1 = ReadString();
-cout << "\nString after Upper:\n";
-S1 = UpperAllString(S1);
-cout << S1 << endl;
-cout << "\nString after Lower:\n";
-S1 = LowerAllString(S1);
-cout << S1 << endl;
-system("pause>0");
+    string S1 = ReadString();
+    cout << "\nString after Upper:\n";
+    S1 = UpperAllString(S1);
+    cout << S1 << endl;
+    cout << "\nString after Lower:\n";
+    S1 = LowerAllString(S1);
+    cout << S1 << endl;
+    system("pause>0");
 }

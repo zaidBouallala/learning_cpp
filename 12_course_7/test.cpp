be8906a53106831f130f6ba6fa43e352732971f0
Zaid Bouallala <zaidbouallala7@gmail.com>
2026-09-22 10:21:05 +0000

    Add age input and output functionality in main function

--- 12_course_7/test.cpp
+++ 12_course_7/test.cpp
@@ -6,6 +6,11 @@ using namespace std;
 
 int main()
 {
+    short age ; 
+    cout << "Enter your age: "<< endl;
+    cin >> age;
+
+    cout << "Your age is : " << age << endl;
     
 
     return 0;
